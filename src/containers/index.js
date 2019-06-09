f9f150a5782c9fa3082174b5e75e9d8228d450d8
steven <stevenn.kulesza@gmail.com>
2019-06-09 14:21:14 -0400

    inital app structure

---
+++ src/containers/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import Table from "../components/table";
+
+export default class Index extends Component {
+  render() {
+    return (
+      <>
+        hello world
+        <Table />
+      </>
+    );
+  }
+}
